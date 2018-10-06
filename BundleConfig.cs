27d8003788609f0b149575b5734a8679e5cfda3b
chaithanya krishna gogineni <gckrishna16@gmail.com>
2018-10-06 11:58:54 -0500

    Add files via upload

---
+++ BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StackOverflowClone
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/jquery-3.3.1.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap1").Include("~/Scripts/umd/popper.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap2").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Styles/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Styles/site").Include("~/Content/Styles.css"));
+            
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
+
+
